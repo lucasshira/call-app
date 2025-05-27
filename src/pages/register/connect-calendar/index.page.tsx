32b1af75faed8d8380d55af3fd84b73c8c1b2e55
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-05-27 17:37:29 +0100

    feat: integrate next-auth for Google authentication

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -3,6 +3,7 @@ import { ArrowRight } from 'phosphor-react'
 
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function ConnectCalendar() {
   return (
@@ -20,7 +21,11 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight size={16} />
           </Button>
